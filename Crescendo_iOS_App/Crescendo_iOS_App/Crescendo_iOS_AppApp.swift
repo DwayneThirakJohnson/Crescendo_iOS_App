2a7b990c9f6ffcabf6a6dd374ca03951dbb3a760
Thirak Buch <buchth@northvilleschools.net>
2024-01-09 19:43:55 -0500

    initial commit

---
+++ Crescendo_iOS_App/Crescendo_iOS_App/Crescendo_iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crescendo_iOS_AppApp.swift
+//  Crescendo_iOS_App
+//
+//  Created by Thirak Buch  on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Crescendo_iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
